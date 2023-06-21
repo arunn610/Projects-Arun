7cd231c4a81937c9f7da0bc1824ec77f693d3dbd
arunn610 <arun610kumar@gmail.com>
2023-06-21 15:06:01 +0530

    vercel done settings

--- new/indian_banks/banking_projects/settings.py
+++ new/indian_banks/banking_projects/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v@ng8#607n$0c*xp5ev1vprphwhyi@7q_4*0+!fo*=e1^+il_e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
